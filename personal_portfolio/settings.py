d7e1177a5b52e9c381e109f38230292eea62f3c5
amjad almohtaseb <amjad.almohtaseb94@gmail.com>
2021-02-09 22:57:38 +0200

    'localsettings'

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+try:
+    from .local_settings import *
+except ImportError:
+    print('looks like no local file , you must be on production')    
